10c694b47a820ef5b883a47debcb32c5fce05f03
Kiran Sinha <kiran.sinha@algoworks.com>
2023-09-14 11:02:21 +0530

    added active class in navigation links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "gatsby";
-import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle } from './layout.module.css';
+import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css';
 
 const Layout = ({ pageTitle, children }: { pageTitle: String; children: JSX.Element|JSX.Element[] }) => {
+  const activeRoute = window.location.pathname;
   return (
     <div className={container}>
       <nav>
         <ul className={navLinks}>
         <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}> Home </Link>
+            <Link to="/" className={navLinkText} activeStyle={{ color: "blue" }} activeClassName="active"> Home </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}> About </Link>
+            <Link to="/about" className={navLinkText} activeStyle={{ color: "blue" }} activeClassName="active"> About </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}> Blog </Link>
+            <Link to="/blog" className={navLinkText} activeStyle={{ color: "blue" }} activeClassName="active"> Blog </Link>
           </li>
         </ul>
       </nav>
